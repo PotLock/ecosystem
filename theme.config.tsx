da4710dc0d4a1529b9c37b808e3b08930b52bc14
codingshot <45281667+codingshot@users.noreply.github.com>
2024-01-22 12:12:12 -0500

    changed readme and footer, and navbar
    probably need to change discord icon

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Potlock Eco</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/PotLock/ecosystem',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://potlock.org/community',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/PotLock/ecosystem',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Potlock',
   },
 }
 
