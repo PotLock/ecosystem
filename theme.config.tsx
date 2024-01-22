03a6cb4f7e4827c1ec953c4e072c496bedcb0df9
codingshot <45281667+codingshot@users.noreply.github.com>
2024-01-22 12:27:36 -0500

    changed index name

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Potlock Eco</span>,
+  logo: <span>🫕 Potlock Eco</span>,
   project: {
     link: 'https://github.com/PotLock/ecosystem',
-  },
-  chat: {
-    link: 'https://potlock.org/community',
   },
   docsRepositoryBase: 'https://github.com/PotLock/ecosystem',
   footer: {
